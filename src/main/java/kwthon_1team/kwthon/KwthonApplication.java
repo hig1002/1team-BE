6a9ea5914a58fa9648e5604076fb6f0614e8b614
hig1002 <hig7463@naver.com>
2024-11-12 17:53:05 +0900

    chore : git ignore setting

---
+++ src/main/java/kwthon_1team/kwthon/KwthonApplication.java
@@ -0,0 +1,13 @@
+package kwthon_1team.kwthon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwthonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwthonApplication.class, args);
+	}
+
+}
