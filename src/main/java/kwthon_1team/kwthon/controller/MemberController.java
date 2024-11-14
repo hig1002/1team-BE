47f82d8dc76b9f399fac15e43743067bfeceab72
seyeon22 <sywolleych@gmail.com>
2024-11-14 19:52:25 +0900

    feat: 회원가입 로직 생성

--- src/main/java/kwthon_1team/kwthon/controller/MemberController.java
+++ src/main/java/kwthon_1team/kwthon/controller/MemberController.java
@@ -1,4 +1,22 @@
 package kwthon_1team.kwthon.controller;
 
+import kwthon_1team.kwthon.common.BaseResponse;
+import kwthon_1team.kwthon.domian.dto.request.AuthRequestDto;
+import kwthon_1team.kwthon.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("/auth/signUp")
+    public BaseResponse signUp(@RequestBody AuthRequestDto authRequestDto) {
+        memberService.join(authRequestDto);
+        return new BaseResponse(HttpStatus.OK.value(), "회원가입 완료되었습니다.");
+    }
 }
--- src/main/java/kwthon_1team/kwthon/service/MemberService.java
+++ src/main/java/kwthon_1team/kwthon/service/MemberService.java
@@ -1,4 +1,25 @@
 package kwthon_1team.kwthon.service;
 
+import kwthon_1team.kwthon.domian.dto.request.AuthRequestDto;
+import kwthon_1team.kwthon.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    private String emailCode;
+
+    @Transactional
+    public void join(AuthRequestDto authRequestDto) {
+        validateAuthRequest(authRequestDto);
+        validateDuplicateEmail(authRequestDto.getEmail());
+        memberRepository.save(converToMember(authRequestDto));
+    }
+
+
 }
