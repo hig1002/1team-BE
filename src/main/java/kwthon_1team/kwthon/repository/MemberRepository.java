78aafb9cecc1f962fc4035fe75462dfb303d6216
hig1002 <129808885+hig1002@users.noreply.github.com>
2024-11-16 16:14:21 +0900

    Update MemberRepository.java

--- src/main/java/kwthon_1team/kwthon/repository/MemberRepository.java
+++ src/main/java/kwthon_1team/kwthon/repository/MemberRepository.java
@@ -14,9 +14,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
     Optional<Member> findByStudentId(Long studentId);
     @Query("SELECT m FROM Member m WHERE " +
-       "(m.name LIKE CONCAT('%', :keyword, '%') " +
-       "OR m.department LIKE CONCAT('%', :keyword, '%') " +
-       "OR CONCAT(m.studentId, '') LIKE CONCAT('%', :keyword, '%'))")
+       "(m.name LIKE CONCAT(:keyword, '%') " +
+       "OR m.department LIKE CONCAT(:keyword, '%') " +
+       "OR CAST(m.studentId AS string) LIKE CONCAT(:keyword, '%'))")
     List<Member> findAllByKeyword(@Param("keyword") String keyword);
 
 }
