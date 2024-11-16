ea6c60a7a87fe59f7733e35287c6ff8b37804053
seyeon22 <103797536+seyeon22@users.noreply.github.com>
2024-11-16 16:50:33 +0900

    Update MemberService.java

--- src/main/java/kwthon_1team/kwthon/service/MemberService.java
+++ src/main/java/kwthon_1team/kwthon/service/MemberService.java
@@ -1,9 +1,7 @@
 package kwthon_1team.kwthon.service;
 
-import kwthon_1team.kwthon.converter.MemberConverter;
 import kwthon_1team.kwthon.domian.dto.request.AuthRequestDto;
 import kwthon_1team.kwthon.domian.dto.response.AuthLoginResponseDto;
-import kwthon_1team.kwthon.domian.dto.response.SearchResponse;
 import kwthon_1team.kwthon.domian.entity.EmailVerification;
 import kwthon_1team.kwthon.domian.entity.Member;
 import kwthon_1team.kwthon.exception.BadRequestException;
@@ -15,7 +13,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 @Transactional
@@ -24,7 +21,6 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final EmailService emailService;
     private final EmailVerificationRepository emailVerificationRepository;
-    private final MemberConverter memberConverter;
 
     @Transactional
     public void join(AuthRequestDto authRequestDto) {
@@ -88,7 +84,7 @@ public class MemberService {
     @Transactional
     public void verifyEmailCode(String email, Integer code) {
 
-        EmailVerification emailVerification = emailVerificationRepository.findByEmail(email)
+        EmailVerification emailVerification = emailVerificationRepository.findLatestByEmailNative(email)
                 .orElseThrow(()->new BaseException(400, "이메일 인증이 필요합니다."));
 
         // 인증 코드 유효 기간 검증
@@ -97,11 +93,32 @@ public class MemberService {
         }
 
         if (emailVerification.getVerificationCode() == null ||
-            !emailVerification.getVerificationCode().equals(code)) {
+                !emailVerification.getVerificationCode().equals(code)) {
             throw new BaseException(400, "인증번호가 일치하지 않습니다.");
         }
         emailVerificationRepository.delete(emailVerification);
+        emailVerificationRepository.flush();
     }
+
+    /*private void saveEmailToSession(String email) {
+        HttpSession session = getHttpSession();
+        session.setAttribute("email", email);
+        System.out.println("이메일이 세션에 저장되었습니다: " + email);
+    }
+
+    private HttpSession getHttpSession() {
+        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        return attr.getRequest().getSession(true);
+    }
+
+    private String getEmailFromSession() {
+        HttpSession session = getHttpSession();
+        String email = (String) session.getAttribute("email");
+        if (email == null) {
+            throw new BaseException(400, "이메일 세션이 존재하지 않습니다.");
+        }
+        return email;
+    }*/
 
     @Transactional
     public AuthLoginResponseDto login(Long studentId, String password) {
@@ -113,10 +130,5 @@ public class MemberService {
     private Member getMemberById(Long studentId) {
         return memberRepository.findByStudentId(studentId).stream().findFirst()
                 .orElseThrow(()->new BaseException(404, "회원가입되지 않은 이메일"));
-    }
-
-    public List<SearchResponse> search(String keyword) {
-        List<Member> memberList = memberRepository.findAllByKeyword(keyword);
-        return memberList.stream().map(memberConverter::toSearchResponse).toList();
     }
 }
