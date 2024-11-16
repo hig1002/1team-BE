be9062008c997a69daff4e066a1f67331859d513
seyeon22 <103797536+seyeon22@users.noreply.github.com>
2024-11-16 16:49:54 +0900

    Update EmailVerificationRepository.java

--- src/main/java/kwthon_1team/kwthon/repository/EmailVerificationRepository.java
+++ src/main/java/kwthon_1team/kwthon/repository/EmailVerificationRepository.java
@@ -2,6 +2,8 @@ package kwthon_1team.kwthon.repository;
 
 import kwthon_1team.kwthon.domian.entity.EmailVerification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +11,11 @@ import java.util.Optional;
 @Repository
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
     Optional<EmailVerification> findByEmail(String email);
+
+
+    @Query("SELECT ev FROM EmailVerification ev WHERE ev.email = :email ORDER BY ev.expirationTime DESC")
+    Optional<EmailVerification> findLatestByEmail(@Param("email") String email);
+
+    @Query(value = "SELECT * FROM email_verification WHERE email = :email ORDER BY expiration_time DESC LIMIT 1", nativeQuery = true)
+    Optional<EmailVerification> findLatestByEmailNative(@Param("email") String email);
 }
