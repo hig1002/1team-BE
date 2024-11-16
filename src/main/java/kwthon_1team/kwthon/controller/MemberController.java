bd354457866229676427cf86a4dd51f6cbee5579
seyeon22 <103797536+seyeon22@users.noreply.github.com>
2024-11-16 15:55:20 +0900

    fix: MemberController 수정

--- src/main/java/kwthon_1team/kwthon/controller/MemberController.java
+++ src/main/java/kwthon_1team/kwthon/controller/MemberController.java
@@ -5,23 +5,17 @@ import kwthon_1team.kwthon.domian.dto.request.AuthDuplicationRequestDto;
 import kwthon_1team.kwthon.domian.dto.request.AuthLoginRequestDto;
 import kwthon_1team.kwthon.domian.dto.request.AuthRequestDto;
 import kwthon_1team.kwthon.domian.dto.request.AuthVerificationEmailRequestDto;
-import kwthon_1team.kwthon.domian.dto.response.SearchResponse;
 import kwthon_1team.kwthon.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
-
-    @GetMapping("/search")
-    public BaseResponse<List<SearchResponse>> search(@RequestParam(value = "keyword") String keyword) {
-        return new BaseResponse(HttpStatus.OK.value(), "검색 결과를 불러왔습니다.", memberService.search(keyword));
-    }
 
     @PostMapping("/auth/signUp")
     public BaseResponse signUp(@RequestBody AuthRequestDto authRequestDto) {
@@ -37,7 +31,7 @@ public class MemberController {
 
     @PostMapping("/auth/emailVerification")
     public BaseResponse verificationEmail(@RequestBody AuthVerificationEmailRequestDto authVerificationEmailRequestDto) {
-        memberService.verifyEmailCode(authVerificationEmailRequestDto.getCode());
+        memberService.verifyEmailCode(authVerificationEmailRequestDto.getEmail(), authVerificationEmailRequestDto.getCode());
         return new BaseResponse(HttpStatus.OK.value(), "사용자 이메일 인증 성공");
     }
 
